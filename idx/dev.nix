dac3a8ac1df0f91c9bb47660f681ef564cd6101d
Toni D <tonidy@outlook.com>
2024-12-28 23:50:32 +0700

    add better toml

--- idx/dev.nix
+++ idx/dev.nix
@@ -18,6 +18,7 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       "denoland.vscode-deno"
+      "tamasfe.even-better-toml"
       "hverlin.mise-vscode"
     ];
     workspace = {
