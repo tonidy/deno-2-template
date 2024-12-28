921657c5c4914c0b15459108c5d6f6805bd98c8b
Toni D <tonidy@outlook.com>
2024-12-28 12:00:40 +0700

    Fix

--- idx/idx-template.nix
+++ idx/idx-template.nix
@@ -7,10 +7,12 @@
 
   bootstrap = ''
         # Install Deno
-        curl -fsSL https://deno.land/install.sh | sh -s ${version} -- --yes
+        curl -fsSL https://deno.land/install.sh | sh -s v2.1.4 -- --yes
 
     	mkdir "$out"
         mkdir -p "$out"/.idx
+
+        echo ${version} > "$out/version.txt"
 
         cp -rf ${./dev.nix} "$out/.idx/dev.nix"
         shopt -s dotglob; cp -r ${../src}/* "$out"
